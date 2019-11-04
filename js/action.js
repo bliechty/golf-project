46c85bd063dc5b11f05c21ad4b1c2aa84354a354
Brady Liechty <berrybloxinator@gmail.com>
2019-11-04 14:00:31 -0700

    commit

--- js/action.js
+++ js/action.js
@@ -20,8 +20,10 @@ $(function() {
                                 <option value='3'>3 Players</option>
                                 <option value='4'>4 Players</option>
                             </select>
-                            <button onclick='displayHoles(${myCourse.data.holes.length});
-                                displayScoreCardInfo(${myCourse.data.holes.length}, $("#selectid${i}").val())'>Select</button>
+                            <button onclick='displayHoles(${myCourse.data.holeCount});
+                                displayScoreCardInfo(${myCourse.data.holeCount}, $("#selectid${i}").val());
+                                displayPar(${JSON.stringify((myCourse.data.holes))}, ${myCourse.data.holeCount});
+                                populateTeeSelect(${JSON.stringify(myCourse.data.holes[0].teeBoxes)})'>Select</button>
                         </div>
                     </div>`);
             });
@@ -30,10 +32,13 @@ $(function() {
 });
 
 function displayHoles (numberOfHoles) {
+    $('.score-card').css('display', 'block');
+    $('.tee-selection').css('display', 'block');
     $('.holes').html('');
     $('.holes').append(`<div id='first-column'>
             <div class='firstColumn'>Hole</div>
         </div>`);
+
     for (let i = 1; i <= numberOfHoles / 2; i++) {
         $('.holes').append(`<div id='col${i}' class='col'>
                 <div class='boxes'>${i}</div>
@@ -41,7 +46,7 @@ function displayHoles (numberOfHoles) {
     }
 
     $('.holes').append(`<div id='out-score' class='scores'>
-            <div class='score-boxes'>Out Score</div>
+            <div class='score-boxes'>Out</div>
         </div>`);
 
     for (let i = numberOfHoles / 2 + 1; i <= numberOfHoles; i++) {
@@ -51,16 +56,16 @@ function displayHoles (numberOfHoles) {
     }
 
     $('.holes').append(`<div id='in-score' class='scores'>
-            <div class='score-boxes'>In Score</div>
+            <div class='score-boxes'>In</div>
         </div>`);
     $('.holes').append(`<div id='total-score' class='scores'>
-            <div class='score-boxes'>Total Score</div>
+            <div class='score-boxes'>Total</div>
         </div>`);
 }
 
 function displayScoreCardInfo (numberOfHoles, numberOfPlayers) {
     for (let i = 1; i <= numberOfPlayers; i++) {
-        $('#first-column').append(`<div class='firstColumn'>Enter Name Here...</div>`);
+        $('#first-column').append(`<div class='firstColumn'>Player ${i} (Click to edit)</div>`);
 
         for (let j = 1; j <= numberOfHoles / 2; j++) {
             $(`#col${j}`).append(`<div id='p${i}h${j}' class='boxes'></div>`);
@@ -74,6 +79,40 @@ function displayScoreCardInfo (numberOfHoles, numberOfPlayers) {
 
         $('#in-score').append(`<div id='inscore${i}' class='score-boxes'></div>`);
         $('#total-score').append(`<div id='totalscore${i}' class='score-boxes'></div>`);
+    }
+}
+
+function displayPar(holesArray, numberOfHoles) {
+    let totalPar = 0;
+    let outPar, inPar;
+    $('#first-column').append(`<div class='firstColumn'>Par</div>`);
+
+    for (let i = 1; i <= numberOfHoles / 2; i++) {
+        totalPar += holesArray[i - 1].teeBoxes[0].par;
+        $(`#col${i}`).append(`<div class='boxes'>${holesArray[i - 1].teeBoxes[0].par}</div>`);
+    }
+
+    outPar = totalPar;
+
+    $('#out-score').append(`<div class='score-boxes'>${outPar}</div>`);
+
+    for (let i = numberOfHoles / 2 + 1; i <= numberOfHoles; i++) {
+        totalPar += holesArray[i - 1].teeBoxes[0].par;
+        $(`#col${i}`).append(`<div class='boxes'>${holesArray[i - 1].teeBoxes[0].par}</div>`);
+    }
+
+    inPar = totalPar - outPar;
+
+    $('#in-score').append(`<div class='score-boxes'>${inPar}</div>`);
+    $('#total-score').append(`<div class='score-boxes'>${totalPar}</div>`);
+}
+
+function populateTeeSelect(currentCourse) {
+    $('#tee-select').html(`<option value='-1'>Pick A Tee</option>`);
+    for (let i = 0; i < currentCourse.length; i++) {
+        if (currentCourse[i].teeType !== 'auto change location') {
+            $('#tee-select').append(`<option value='${currentCourse[i].teeType}'>${currentCourse[i].teeType}</option>`);
+        }
     }
 }
 
